aab6c1f02715ce8c9ee640fb0c2a81d5d1af5c9b
mikuA <akmt920@gmail.com>
2022-02-25 20:11:04 +0900

    homeを追加しました

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,63 @@
 <template>
-  <h1>Home</h1>
-  <div>ここにそれぞれのアプリに行けるリンクを設置する</div>
+  <div class="home">
+    <div class="left">
+      <div class="leftbox">
+        <div class="teamname">こんにちは</div>
+        <div class="name">
+          <p>Satsuki Fukushima</p>
+          <p>Miku Akimoto</p>
+          <p>EEri Morifuji</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="right">
+      <div class="rightbox">
+        <p>クイズ</p>
+        <p>Vueパレット</p>
+        <p>メモ</p>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script></script>
+
+<style>
+.home {
+  display: flex;
+  width: 100%;
+}
+.right {
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+}
+.left {
+  width: 50%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+}
+.leftbox {
+  position: absolute;
+  top: 40%;
+  left: 30%;
+}
+.teamname {
+  font-size: 30px;
+  margin-bottom: 50px;
+  font-weight: bold;
+}
+.name {
+  font-size: 20px;
+}
+.rightbox {
+  position: absolute;
+  top: 40%;
+  left: 60%;
+  font-size: 30px;
+}
+</style>
